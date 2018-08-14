6bca66c28660ec1815e36ea12052852b73c80604
B. W. Avants <BWAvants@users.noreply.github.com>
2018-08-14 12:09:25 -0500

    initial scripts upload - fixed

--- FC4D_CameraGrabber.py
+++ FC4D_CameraGrabber.py
@@ -7,6 +7,8 @@ import numpy as np
 # from multiprocessing import Process, Event
 import socket
 from select import select
+from threading import Thread, Event
+from queue import Queue, Full
 
 
 class StopGuard:
@@ -111,104 +113,132 @@ class PylonCam:
             pass
 
 
-if os.path.exists('./FC4D.af_inet'):
-    try:
-        os.unlink('./FC4D.af_inet')
-    except OSError as e:
-        if os.path.exists('./FC4D.af_inet'):
-            logging.error(e)
-            raise e
-serveSock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-serveSock.bind('./FC4D.af_inet')
-serveSock.listen(10)
+def manage_client(new_client, notification_event, message_queue, stop_flag):
+    connected = True
+    stream = ''
+    while connected and not stop_flag:
+        r, w, e = select([new_client, ], [], [], 0.01)
+        for c in r:
+            data = c.recv(1024)
+            if len(data) == 0:
+                connected = False
+                try:
+                    c.shutdown(socket.SHUT_RDWR)
+                finally:
+                    try:
+                        c.close()
+                    finally:
+                        logging.info('Client Disconnected: ' + c.getpeername())
+                continue
+            stream += data.decode('utf-8')
+            while '\n' in stream and not stop_flag:
+                message, stream = stream.split('\n')
+                put_in_queue = False
+                while not put_in_queue and not stop_flag:
+                    try:
+                        message_queue.put(message, timeout=0.01)
+                    except Full:
+                        logging.debug(__name__ + ': Message Queue Full')
+                    except Exception as e:
+                        logging.error(e)
+                    else:
+                        put_in_queue = True
+                notification_event.set()
 
-running = True
-clients = []
 
-while running and not stoppingGuard.stop:
-    r, w, e = select([serveSock, ], [], [], 0)
-    for request in r:
-        client, addr = serveSock.accept()
-        clients.append(client)
-        logging.warning('Client connected @' + str(addr))
-    r, w, e = select([clients, ], [], [], 0)
+if __name__ == '__main__':
+    serveSock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+    serveSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    serveSock.bind(('127.0.0.1', 0xFC4D))
+    serveSock.listen(128)
 
-serveSock.shutdown(socket.SHUT_RDWR)
-serveSock.close()
-os.unlink('./FC4D.af_inet')
-sys.exit()
+    running = True
+    clients = []
+    notification_event = Event()
+    message_queue = Queue()
 
-'''
-PixelType_BGR10V1packed ~ 35651612
-PixelType_BGR10V2packed ~ 35651613
-PixelType_BGR10packed ~ 36700185
-PixelType_BGR12packed ~ 36700187
-PixelType_BGR8packed ~ 35127317
-PixelType_BGRA8packed ~ 35651607
-PixelType_BayerBG10 ~ 17825807
-PixelType_BayerBG10p ~ 17432658
-PixelType_BayerBG12 ~ 17825811
-PixelType_BayerBG12Packed ~ 17563693
-PixelType_BayerBG12p ~ 17563731
-PixelType_BayerBG16 ~ 17825841
-PixelType_BayerBG8 ~ 17301515
-PixelType_BayerGB10 ~ 17825806
-PixelType_BayerGB10p ~ 17432660
-PixelType_BayerGB12 ~ 17825810
-PixelType_BayerGB12Packed ~ 17563692
-PixelType_BayerGB12p ~ 17563733
-PixelType_BayerGB16 ~ 17825840
-PixelType_BayerGB8 ~ 17301514
-PixelType_BayerGR10 ~ 17825804
-PixelType_BayerGR10p ~ 17432662
-PixelType_BayerGR12 ~ 17825808
-PixelType_BayerGR12Packed ~ 17563690
-PixelType_BayerGR12p ~ 17563735
-PixelType_BayerGR16 ~ 17825838
-PixelType_BayerGR8 ~ 17301512
-PixelType_BayerRG10 ~ 17825805
-PixelType_BayerRG10p ~ 17432664
-PixelType_BayerRG12 ~ 17825809
-PixelType_BayerRG12Packed ~ 17563691
-PixelType_BayerRG12p ~ 17563737
-PixelType_BayerRG16 ~ 17825839
-PixelType_BayerRG8 ~ 17301513
-PixelType_Double ~ -2127560448
-PixelType_Mono10 ~ 17825795
-PixelType_Mono10p ~ 17432646
-PixelType_Mono10packed ~ 17563652
-PixelType_Mono12 ~ 17825797
-PixelType_Mono12p ~ 17563719
-PixelType_Mono12packed ~ 17563654
-PixelType_Mono16 ~ 17825799
-PixelType_Mono1packed ~ -2130640884
-PixelType_Mono2packed ~ -2130575347
-PixelType_Mono4packed ~ -2130444274
-PixelType_Mono8 ~ 17301505
-PixelType_Mono8signed ~ 17301506
-PixelType_RGB10packed ~ 36700184
-PixelType_RGB10planar ~ 36700194
-PixelType_RGB12V1packed ~ 35913780
-PixelType_RGB12packed ~ 36700186
-PixelType_RGB12planar ~ 36700195
-PixelType_RGB16packed ~ 36700211
-PixelType_RGB16planar ~ 36700196
-PixelType_RGB8packed ~ 35127316
-PixelType_RGB8planar ~ 35127329
-PixelType_RGBA8packed ~ 35651606
-PixelType_Undefined ~ -1
-PixelType_YUV411packed ~ 34340894
-PixelType_YUV422_YUYV_Packed ~ 34603058
-PixelType_YUV422packed ~ 34603039
-PixelType_YUV444packed ~ 35127328
-'''
-'''
-{'PixelType_BGR8packed': 35127317,
- 'PixelType_BGRA8packed': 35651607,
- 'PixelType_Mono16': 17825799,
- 'PixelType_Mono8': 17301505,
- 'PixelType_RGB16packed': 36700211,
- 'PixelType_RGB16planar': 36700196,
- 'PixelType_RGB8packed': 35127316,
- 'PixelType_RGB8planar': 35127329}
-'''
+    while running and not stoppingGuard.stop:
+        r, w, e = select([serveSock, ], [], [], 0)
+        for request in r:
+            client, addr = serveSock.accept()
+            new_client = Thread(target=manage_client, args=(client, notification_event, message_queue, running))
+            new_client.start()
+            clients.append(client)
+            logging.info('Client connected @' + str(addr))
+
+    serveSock.shutdown(socket.SHUT_RDWR)
+    serveSock.close()
+    sys.exit()
+
+
+# PixelType_BGR10V1packed ~ 35651612
+# PixelType_BGR10V2packed ~ 35651613
+# PixelType_BGR10packed ~ 36700185
+# PixelType_BGR12packed ~ 36700187
+# PixelType_BGR8packed ~ 35127317
+# PixelType_BGRA8packed ~ 35651607
+# PixelType_BayerBG10 ~ 17825807
+# PixelType_BayerBG10p ~ 17432658
+# PixelType_BayerBG12 ~ 17825811
+# PixelType_BayerBG12Packed ~ 17563693
+# PixelType_BayerBG12p ~ 17563731
+# PixelType_BayerBG16 ~ 17825841
+# PixelType_BayerBG8 ~ 17301515
+# PixelType_BayerGB10 ~ 17825806
+# PixelType_BayerGB10p ~ 17432660
+# PixelType_BayerGB12 ~ 17825810
+# PixelType_BayerGB12Packed ~ 17563692
+# PixelType_BayerGB12p ~ 17563733
+# PixelType_BayerGB16 ~ 17825840
+# PixelType_BayerGB8 ~ 17301514
+# PixelType_BayerGR10 ~ 17825804
+# PixelType_BayerGR10p ~ 17432662
+# PixelType_BayerGR12 ~ 17825808
+# PixelType_BayerGR12Packed ~ 17563690
+# PixelType_BayerGR12p ~ 17563735
+# PixelType_BayerGR16 ~ 17825838
+# PixelType_BayerGR8 ~ 17301512
+# PixelType_BayerRG10 ~ 17825805
+# PixelType_BayerRG10p ~ 17432664
+# PixelType_BayerRG12 ~ 17825809
+# PixelType_BayerRG12Packed ~ 17563691
+# PixelType_BayerRG12p ~ 17563737
+# PixelType_BayerRG16 ~ 17825839
+# PixelType_BayerRG8 ~ 17301513
+# PixelType_Double ~ -2127560448
+# PixelType_Mono10 ~ 17825795
+# PixelType_Mono10p ~ 17432646
+# PixelType_Mono10packed ~ 17563652
+# PixelType_Mono12 ~ 17825797
+# PixelType_Mono12p ~ 17563719
+# PixelType_Mono12packed ~ 17563654
+# PixelType_Mono16 ~ 17825799
+# PixelType_Mono1packed ~ -2130640884
+# PixelType_Mono2packed ~ -2130575347
+# PixelType_Mono4packed ~ -2130444274
+# PixelType_Mono8 ~ 17301505
+# PixelType_Mono8signed ~ 17301506
+# PixelType_RGB10packed ~ 36700184
+# PixelType_RGB10planar ~ 36700194
+# PixelType_RGB12V1packed ~ 35913780
+# PixelType_RGB12packed ~ 36700186
+# PixelType_RGB12planar ~ 36700195
+# PixelType_RGB16packed ~ 36700211
+# PixelType_RGB16planar ~ 36700196
+# PixelType_RGB8packed ~ 35127316
+# PixelType_RGB8planar ~ 35127329
+# PixelType_RGBA8packed ~ 35651606
+# PixelType_Undefined ~ -1
+# PixelType_YUV411packed ~ 34340894
+# PixelType_YUV422_YUYV_Packed ~ 34603058
+# PixelType_YUV422packed ~ 34603039
+# PixelType_YUV444packed ~ 35127328
+#
+# {'PixelType_BGR8packed': 35127317,
+#  'PixelType_BGRA8packed': 35651607,
+#  'PixelType_Mono16': 17825799,
+#  'PixelType_Mono8': 17301505,
+#  'PixelType_RGB16packed': 36700211,
+#  'PixelType_RGB16planar': 36700196,
+#  'PixelType_RGB8packed': 35127316,
+#  'PixelType_RGB8planar': 35127329}
